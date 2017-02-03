97730519798c6b3e46c3887e3981bff7f66e5263
Bryce Boe <bbzbryce@gmail.com>
2017-02-02 22:18:17 -0800

    Configure rabbit_util script when building the image

---
+++ build_scripts/configure_rabbit_util.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+
+# Create bin directory
+mkdir -p bin
+
+# Make rabbit_util executable and relocate to directory on path
+chmod +x rabbit_util
+mv rabbit_util bin/
+
+# Install python packages needed for rabbit_util
+sudo /usr/local/bin/pip install docopt pika
